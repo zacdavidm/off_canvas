d7aea3afd30a390c5baa60ed7f9fe0f8f5674f14
zacdavidm <zacdavidm@gmail.com>
2014-05-08 15:37:43 -0400

    updated js to check for toggle elements and moved init to page bottom

--- foundation/vanillajs_off_canvas.js
+++ foundation/vanillajs_off_canvas.js
@@ -1,5 +1,5 @@
-vanillajs_offcanvas = {
-  name : 'vanillajs offcanvas',
+vanillajs_off_canvas = {
+  name : 'vanillajs off canvas',
   
   version : '5.2.2x1',
   init : function(){
@@ -8,23 +8,28 @@ vanillajs_offcanvas = {
   
   events : function(){
     var self = this;
-    
-    document.getElementsByClassName('left-off-canvas-toggle')[0].onclick = function(e){
-      obj=self.closest_wrap(e);
-      self.click_toggle_class(obj, 'move-right');
-    };
-    
-    document.getElementsByClassName('right-off-canvas-toggle')[0].onclick = function(e){
-      obj=self.closest_wrap(e);
-      self.click_toggle_class(obj, 'move-left');
-    };
-    
-    document.getElementsByClassName('exit-off-canvas')[0].onclick = function(e){
-      obj=self.closest_wrap(e);
-      self.click_remove_class(obj, 'move-right');
-      self.click_remove_class(obj, 'move-left');
-    };
-    console.log('events');
+    left_toggles=document.getElementsByClassName('left-off-canvas-toggle')
+    if(left_toggles.length>0){
+      left_toggles[0].onclick = function(e){
+        obj=self.closest_wrap(e);
+        self.click_toggle_class(obj, 'move-right');
+      };
+    }
+    right_toggles=document.getElementsByClassName('right-off-canvas-toggle')
+    if(right_toggles.length>0){
+      right_toggles[0].onclick = function(e){
+        obj=self.closest_wrap(e);
+        self.click_toggle_class(obj, 'move-left');
+      };
+    }
+    exit_elements=document.getElementsByClassName('exit-off-canvas')
+    if(exit_elements.length>0){
+      exit_elements[0].onclick = function(e){
+        obj=self.closest_wrap(e);
+        self.click_remove_class(obj, 'move-right');
+        self.click_remove_class(obj, 'move-left');
+      };
+    }
   },
   
   click_toggle_class : function(obj, class_name){
@@ -49,5 +54,3 @@ vanillajs_offcanvas = {
   }
   
 };
-
-vanillajs_offcanvas.init();
